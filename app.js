bffb9e82b42caa5013a754e0994a080b89163413
Matias <matdnunez@gmail.com>
2022-12-08 03:36:57 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const port = 3000;
+
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname,'public')));
+
+app.get('/',(req,res) => res.sendFile(path.resolve(__dirname,'views','home.html')));
+
+
+
+
+app.listen(port, () => `Servidor corriendo en http://localhost:${port}`);
