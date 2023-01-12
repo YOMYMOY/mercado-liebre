38ac5bed136a154db29d9d771fec2d3c3023ffab
YOMYMOY <matdnunez@gmail.com>
2023-01-12 19:59:54 -0300

    agrega maquetado y estructura de login y register

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(express.static(path.resolve(__dirname,'public')));
 
 app.get('/',(req,res) => res.sendFile(path.resolve(__dirname,'views','home.html')));
-
-
+app.get('/register.html',(req,res) => res.sendFile(path.resolve(__dirname,'views','register.html')));
+app.get('/login.html',(req,res) => res.sendFile(path.resolve(__dirname,'views','login.html')));
 
 
 app.listen(port, () => console.log(`Servidor corriendo en http://localhost:${port}`));
